fba0db73ce4cc65441500ddede7455f93d741a62
Aliaksei Stratsilatau <sa@uavos.com>
2020-01-04 14:50:27 +0300

    mandala tree gcs test

--- src/Plugins/Windows/MandalaTree/MandalaTree.cpp
+++ src/Plugins/Windows/MandalaTree/MandalaTree.cpp
@@ -24,7 +24,6 @@
 #include <App/App.h>
 #include <App/AppRoot.h>
 #include <MandalaDict.h>
-#include <MandalaMeta.h>
 //=============================================================================
 MandalaTree::MandalaTree(Fact *parent)
     : Fact(parent,
@@ -34,7 +33,7 @@ MandalaTree::MandalaTree(Fact *parent)
            Group,
            "hexagon-multiple")
 {
-    qDebug() << mandala::meta::sns::nav::ins::gyro::title;
+    qDebug() << mandala::sns::nav::ins::gyro::title;
 
     /*mandala::data m;
     qDebug() << m.sns.title;
@@ -45,21 +44,52 @@ MandalaTree::MandalaTree(Fact *parent)
     qDebug() << m.sns.nav.ins.mag.z;
     qDebug() << m.sns.nav.ins.mag.z.uid;*/
 
-    static mandala::Text<mandala::meta::sns::nav::ins::acc::y> txt;
+    static mandala::Text<mandala::sns::nav::ins::acc::y> txt;
     qDebug() << txt.title();
 
-    class mandala::meta::sns::nav::ins::mag::y hy;
+    struct mandala::sns::nav::ins::mag::y hy;
     qDebug() << hy.title << hy;
-    hy.setValue(0.551f);
+    hy.set(0.551f);
     qDebug() << hy.title << hy;
 
-    mandala::value<mandala::meta::sns::nav::ins::mag::y> v_hy;
-    qDebug() << v_hy.title << v_hy;
-    v_hy.setValue(0.101f);
-    qDebug() << v_hy.title << v_hy;
+    {
+        mandala::Value<mandala::sns::nav::ins::mag::y> v_hy;
+        qDebug() << "Value:" << v_hy.title << v_hy;
+        v_hy.set(0.101f);
+        qDebug() << "Value:" << v_hy.title << v_hy;
+        mandala::Value<mandala::sns::nav::ins::mag::y> v_hy2;
+        qDebug() << "Value:" << v_hy2.title << v_hy2;
+    }
+
+    {
+        mandala::StaticValue<mandala::sns::nav::ins::mag::y> s_hy;
+        qDebug() << "StaticValue:" << s_hy.data().title << s_hy.data();
+        s_hy.data().set(0.101f);
+        qDebug() << "StaticValue:" << s_hy.data().title << s_hy.data();
+        mandala::StaticValue<mandala::sns::nav::ins::mag::y> s_hy2;
+        qDebug() << "StaticValue:" << s_hy2.data().title << s_hy2.data();
+    }
 
     size_t sz = sizeof(mandala::dict);
     size_t sz1 = sizeof(*mandala::dict);
     qDebug() << sz << sz1 << sz / sz1;
+
+    Fact *group = this;
+    uint8_t level = 0;
+    for (size_t i = 0; i < (sizeof(mandala::dict) / sizeof(*mandala::dict)); ++i) {
+        const mandala::dict_meta_t &d = mandala::dict[i];
+        if (d.group) {
+            //move group to upper level
+            for (; level > d.level; --level)
+                group = group->parentFact();
+            level = d.level + 1;
+            group = new Fact(group, d.name, d.name, d.title);
+            continue;
+        }
+        Fact *f = new Fact(group, d.name, d.name, d.title);
+        QString alias(d.alias);
+        if (!alias.isEmpty())
+            f->setValue(alias);
+    }
 }
 //============================================================================
